37ad9777674749272985a150e4558fe660a2357d
Andreas Sakellariou <andreas.sakellariou@gmail.com>
2021-03-25 07:20:55 +0200

    Added getVesselsInPort-Fixed bug when speed=0 or course=0

--- api.js
+++ api.js
@@ -28,7 +28,7 @@ function parsePosition(position) {
 
 const headers = {
   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3703.0 Safari/537.36',
- // 'Content-Type' : 'application/x-www-form-urlencoded',
+  'Content-Type' : 'application/x-www-form-urlencoded',
   'cache-control': 'max-age=0',
   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
   'upgrade-insecure-requests':1,
@@ -147,7 +147,7 @@ function getLocationFromMT(mmsi, cb) {
               //const $ = cheerio.load(html);
               console.log(timestamp, speed ,course ,latitude ,longitude)
 
-              if (timestamp && speed && course && latitude && longitude) {
+              if (timestamp && latitude && longitude) {
                 cb(
                   parsePosition({
                     error: null,
